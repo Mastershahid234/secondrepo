39bfd9194d2df31501bcfaab1d31af1e4bcef606
jollyllb123 <shahid06h@gmail.com>
2023-03-19 15:07:51 +0530

    last commit

---
+++ javagit/arr1.java
@@ -0,0 +1,15 @@
+package hello;
+import java.util.*;
+public class arr1 {
+	  public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+  int i=5;
+  System.out.println(--i);
+	  }
+}
+
+
+	
+
+
